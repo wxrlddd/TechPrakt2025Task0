aef3f0b03a16a2938a62b74c917163e902e6a307
wxrld <ipz232_alm@student.ztu.edu.ua>
2025-02-04 21:48:57 +0200

    added console project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть перше число: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введіть друге число: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double sum = num1 + num2;
+        Console.WriteLine($"Сума чисел: {sum}");
+    }
+}
